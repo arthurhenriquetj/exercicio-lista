2feb7bbce3b8140ff2e9d463849ee1397187349a
arthurhenriquetj <arthurhenriquetj@gmail.com>
2018-11-10 17:13:50 -0200

    base do programa comentada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,38 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>(); //instanciação da lista usando a classe Employee criada
+		
+		System.out.println("Hoy many employees will be added?");
+		int numE = sc.nextInt();
+		
+		for(int i=0;i<numE;i++) {
+			System.out.println("Employee #:" + (i+1));
+			System.out.print("ID: ");
+			int id = sc.nextInt();
+			System.out.print("Name: ");
+			sc.nextLine(); // next line necessário para que variável name receba o proximo nextline
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			double salary = sc.nextDouble();
+			list.add(new Employee(id, name, salary)); // adicionando os dados colhidos à lista referenciada na classe employee		
+		}
+		
+		
 
+		sc.close();
 	}
 
 }
